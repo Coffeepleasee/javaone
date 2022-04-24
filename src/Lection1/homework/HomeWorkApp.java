69ebfcd710447b90acae25be4af13037b4e43e1a
coffe_please <saleykina@gmail.com>
2022-04-24 22:07:38 +0300

    Create new project

---
+++ src/Lection1/homework/HomeWorkApp.java
@@ -0,0 +1,15 @@
+package Lection1.homework;
+
+public class HomeWorkApp {
+
+    public static void main(String [] args) {
+        System.out.println("Let's start!");
+    }
+
+        public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
